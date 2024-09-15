b4b61cfa6e1039f7bb53799a52096ecf9abe4d80
AFraderaSola <albertfradera85@hotmail.com>
2024-09-15 13:44:23 +0200

    v.0.0.3 - Chess Barplot
    I have added the first plot to the app, the chess barplot. Other minor UI improvements have also been added.

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ library(gitlink)
 library(shinyBS)
 library(shinythemes)
 library(googlesheets4)
+library(plotly)
 set.seed(666)
 
 ############################
@@ -30,41 +31,57 @@ ui <- navbarPage(title = "Game board database",
                  theme = shinytheme("lumen"),
                  windowTitle = "Game board database",
                  tabPanel("Chess",
-                          fluidRow(column(2,
-                                          textInput(
-                                            inputId = "chess_date",
-                                            label = "Input Date (YYYYMMDD):"
-                                          )),
-                                   column(2,
-                                          selectInput(inputId = "chess_dayeperiod", 
-                                                      label = "Select Day period:", 
-                                                      choices = c("Morning", "Afternoon", "Evening", "Night")
-                                          )),
-                                   column(2,
-                                          selectInput(inputId = "chess_player", 
-                                                      label = "Select Player:", 
-                                                      choices = c("Albert", "Emily")
-                                          )),
-                                   
-                                   column(2,
-                                          selectInput(inputId = "chess_set", 
-                                                      label = "Select Set:", 
-                                                      choices = c("White", "Black")
-                                          )),
-                                   column(2,
-                                          selectInput(inputId = "chess_victory", 
-                                                      label = "Did you win?", 
-                                                      choices = c(T, F)
-                                          ))),
-                          fluidRow(column(2,
-                                          actionButton("submit",
-                                                       "Submit"))),
-                          fluidRow(column(12,
-                                          dataTableOutput("chess_df")))))
+                          tabsetPanel(
+                            tabPanel("Overview",
+                                     fluidRow(column(12,
+                                                     tags$hr(style="border-color: darkgrey;"))),
+                                     fluidRow(column(12,
+                                                     HTML("<h4><b>Victory barplot</b></h4>"))),
+                                     fluidRow(column(12,
+                                                     plotlyOutput("chess_Barplot",height = "1000px")
+                                                     )),
+                                     fluidRow(column(12,
+                                                     tags$hr(style="border-color: darkgrey;")))),
+                            tabPanel("Data input",
+                                     fluidRow(column(12,
+                                                     tags$hr(style="border-color: darkgrey;"))),
+                                     fluidRow(column(2,
+                                                     textInput(
+                                                       inputId = "chess_date",
+                                                       label = "Input Date (YYYYMMDD):"
+                                                     )),
+                                              column(2,
+                                                     selectInput(inputId = "chess_dayeperiod", 
+                                                                 label = "Select Day period:", 
+                                                                 choices = c("Morning", "Afternoon", "Evening", "Night")
+                                                     )),
+                                              column(2,
+                                                     selectInput(inputId = "chess_player", 
+                                                                 label = "Select Player:", 
+                                                                 choices = c("Albert", "Emily")
+                                                     )),
+                                              
+                                              column(2,
+                                                     selectInput(inputId = "chess_set", 
+                                                                 label = "Select Set:", 
+                                                                 choices = c("White", "Black")
+                                                     )),
+                                              column(2,
+                                                     selectInput(inputId = "chess_victory", 
+                                                                 label = "Did you win?", 
+                                                                 choices = c(T, F)
+                                                     ))),
+                                     fluidRow(column(2,
+                                                     actionButton("chess_submit",
+                                                                  "Submit"))),
+                                     fluidRow(column(12,
+                                                     dataTableOutput("chess_df"))),
+                                     fluidRow(column(12,
+                                                     tags$hr(style="border-color: darkgrey;")))))))
 
 server <- function(input, output) {
   
-  results <- eventReactive(input$submit, {
+  chess_inputData <- eventReactive(input$chess_submit, {
     
     data <- data.frame(
       Date = input$chess_date,
@@ -79,8 +96,41 @@ server <- function(input, output) {
   })
   
   output$chess_df <- renderDataTable({
-    results()
+    chess_inputData()
     read_sheet(sheet_id)
+  })
+  
+  chess_Data <- reactive({
+    read_sheet(sheet_id,sheet = "Chess")
+  })
+  
+  output$chess_Barplot <- renderPlotly({
+    
+    chess_df <- chess_Data()
+    
+    chess_bp_df <- chess_df |> 
+      group_by(Player, Victory) |> 
+      tally()
+    
+    chess_bp_df$Victory <- factor(x = chess_bp_df$Victory, levels = c("FALSE", "TRUE"))
+    
+    chess_bp <- ggplot(chess_bp_df, aes(fill=Victory, y=n, x=Player)) + 
+      geom_bar(position="stack", stat="identity")+
+      ylab("Number of matches")+
+      scale_fill_brewer(palette = "Accent",
+                        direction = -1,
+                        name="",
+                        labels=c("Loss", "Victory"))+
+      scale_y_continuous(limits = c(0,max(chess_bp_df$n)),breaks = seq(0, max(chess_bp_df$n), by = 1))+
+      theme_minimal()+
+      theme(axis.text=element_text(size=18),axis.title=element_text(size=20,face="bold"))+
+      theme(legend.title = element_text(size = 16, face = "bold"),
+            legend.text = element_text(size = 14),
+            legend.key.size = unit(1, 'cm'),
+            legend.position = "none")
+    
+    print(chess_bp)
+    
   })
 
 }
